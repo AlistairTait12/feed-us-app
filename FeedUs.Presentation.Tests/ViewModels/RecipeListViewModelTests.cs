81c2609f53e0a4cc14ad318d674689e13dde83b3
Alistair Tait <alistair.tait1@gmail.com>
2024-05-08 14:14:43 +0100

    Added RecipesViewModel to view list of recipes

---
+++ FeedUs.Presentation.Tests/ViewModels/RecipeListViewModelTests.cs
@@ -0,0 +1,47 @@
+﻿using FakeItEasy;
+using FeedUs.Presentation.DataAccess;
+using FeedUs.Presentation.Models;
+using FeedUs.Presentation.ViewModels;
+using FluentAssertions;
+using System.Diagnostics.CodeAnalysis;
+
+namespace FeedUs.Presentation.Tests.ViewModels;
+
+[ExcludeFromCodeCoverage]
+[TestFixture]
+public class RecipeListViewModelTests
+{
+    private IDataAccess _dataAccess;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _dataAccess = A.Fake<IDataAccess>();
+        var recipes = new List<Recipe>
+        {
+            new () { Title = "Avocado Smash" },
+            new () { Title = "Pasta" }
+        };
+        A.CallTo(() => _dataAccess.GetRecipesAsync()).Returns(recipes);
+    }
+
+    [Test]
+    public async Task LoadRecipesAsync_PopulatesRecipesInViewModel()
+    {
+        // Arrange
+        var expected = new List<Recipe>
+        {
+            new () { Title = "Avocado Smash" },
+            new () { Title = "Pasta" }
+        };
+
+        var viewModel = new RecipeListViewModel(_dataAccess);
+
+        // Act
+        await viewModel.LoadRecipesAsync();
+
+        // Assert
+        viewModel.Recipes.Should().BeEquivalentTo(expected,
+            assertionOptions => assertionOptions.WithStrictOrdering());
+    }
+}
---
+++ FeedUs.Presentation/ViewModels/RecipeListViewModel.cs
@@ -0,0 +1,19 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using FeedUs.Presentation.DataAccess;
+using FeedUs.Presentation.Models;
+using System.Collections.ObjectModel;
+
+namespace FeedUs.Presentation.ViewModels;
+
+public partial class RecipeListViewModel : ObservableObject
+{
+    private readonly IDataAccess _dataAccess;
+
+    public ObservableCollection<Recipe> Recipes { get; set; } = new();
+
+    public RecipeListViewModel(IDataAccess dataAccess) => _dataAccess = dataAccess;
+
+    [RelayCommand]
+    public async Task LoadRecipesAsync() => Recipes = new(await _dataAccess.GetRecipesAsync());
+}
