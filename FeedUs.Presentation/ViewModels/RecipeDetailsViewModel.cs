18e1a65db9cebb7238fbbb890ee00f6f8638b9d7
Alistair Tait <alistair.tait1@gmail.com>
2024-05-13 21:26:22 +0100

    Added some logic to be able to delete a recipe from the recipe details page

--- FeedUs.Presentation/ViewModels/RecipeDetailsViewModel.cs
+++ FeedUs.Presentation/ViewModels/RecipeDetailsViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using FeedUs.Presentation.DataAccess;
 using FeedUs.Presentation.Models;
 
 namespace FeedUs.Presentation.ViewModels;
@@ -7,9 +8,20 @@ namespace FeedUs.Presentation.ViewModels;
 [QueryProperty(nameof(Recipe), "Recipe")]
 public partial class RecipeDetailsViewModel : ObservableObject
 {
+    private readonly IDataAccess _dataAccess;
+
+    public RecipeDetailsViewModel(IDataAccess dataAccess) => _dataAccess = dataAccess;
+
     [ObservableProperty]
     Recipe recipe;
 
     [RelayCommand]
     public async Task GoBackAsync() => await Shell.Current.GoToAsync("..");
+
+    [RelayCommand]
+    public async Task DeleteRecipeAsync()
+    {
+        await _dataAccess.DeleteRecipeAsync(recipe.Id);
+        await Shell.Current.GoToAsync("..");
+    }
 }
